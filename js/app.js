b81b9df98e1a0741a8e64603972a1eaca88c0ead
Anna Mieszczyńska <angem1@wp.pl>
2018-08-07 06:54:16 +0200

    partly done

---
+++ js/app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const subMenu = document.getElementsByClassName('subMenu');
+    const mainMenu = document.getElementsByClassName('mainMenu');
+
+});
